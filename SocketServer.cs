77e22f1a7c55be3db3acfba7c4ddff1fe66df442
Konstantinos Paparas <kelsos86@gmail.com>
2011-10-06 23:51:05 +0300

    Adds changes on the socket server based on some code by Steven. Adds initial work on the protocol.

--- SocketServer.cs
+++ SocketServer.cs
@@ -1,168 +1,219 @@
 ﻿using System;
 using System.Threading;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Configuration;
-using System.Text;
 
 namespace MusicBeePlugin
 {
-    class SocketServer
+    public class SocketServer
     {
-        private static bool _continueListening = true;
-        private TcpListener tcpListener;
-        private Thread listenThread;
+        private static bool _isStopping;
+        private static TcpListener _listener;
+        private static Thread _listenerThread;
+        private static ManualResetEvent _clientConnected;
 
-        //Currently played song
+        private static Socket _clientSocket;
+        private static Plugin _plugin;
 
-        private static string _artist;
-        private static string _album;
-        private static string _song;
-        private static string _year;
-        private static string _imageData;
-        private static bool _songChanged;
-
-        public bool continueListening
+        public SocketServer(Plugin plugin)
         {
-            get
+            _plugin = plugin;
+        }
+
+        public static bool Stop()
+        {
+            _isStopping = true;
+            if (_listenerThread != null && _listenerThread.IsAlive)
             {
-                return _continueListening;
+                _listenerThread.Abort();
             }
-            set
+            _listenerThread = null;
+            if (_listener != null)
             {
-                _continueListening = value;
+                _listener.Stop();
+                _listener = null;
+            }
+            return true;
+        }
+
+        public static bool Start()
+        {
+            _isStopping = false;
+            try
+            {
+                //if (UserConfiguration.SystemOptions.ExternalHosts == null || UserConfiguration.SystemOptions.ExternalHosts.Count == 0 || (UserConfiguration.SystemOptions.ExternalHosts.Count == 1 && UserConfiguration.SystemOptions.ExternalHosts(0) == "127.0.0.1"))
+                //{
+                //    _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), UserConfiguration.SystemOptions.ServerPort);
+                //}
+                //else
+                //{
+                //    _listener = new TcpListener(IPAddress.Any, UserConfiguration.SystemOptions.ServerPort);
+                //}
+                _listener = new TcpListener(IPAddress.Any, 9999);
+                _listener.Start();
+                _clientConnected = new ManualResetEvent(false);
+                _listenerThread = new Thread(ListenForClients);
+                _listenerThread.IsBackground = true;
+                _listenerThread.Priority = ThreadPriority.Lowest;
+                _listenerThread.Start();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //ErrorHandler.LogError(ex);
+                return false;
             }
         }
 
-        public string artist
+        private static void ListenForClients()
         {
-            get
-            {
-                return _artist;
-            }
-            set
-            {
-                _artist = value;
-            }
-        }
-
-        public string album
-        {
-            get
-            {
-                return _album;
-            }
-            set
-            {
-                _album = value;
-            }
-        }
-
-        public string song
-        {
-            get
-            {
-                return _song;
-            }
-            set
-            {
-                _song = value;
-            }
-        }
-
-        public string year
-        {
-            get
-            {
-                return _year;
-            }
-            set
-            {
-                _year = value;
-            }
-        }
-
-        public bool songChanged
-        {
-            get
-            {
-                return _songChanged;
-            }
-            set
-            {
-                _songChanged = value;
-            }
-        }
-
-        public string imageData
-        {
-           get
-           {
-               return _imageData;
-           }
-           set
-           {
-              _imageData = value;
-           }
-        }
-        //currently played song info end
-
-        public SocketServer()
-        {
-            this.tcpListener = new TcpListener(IPAddress.Any, 9741);
-            this.listenThread = new Thread(new ThreadStart(ListenForClients));
-            this.listenThread.Start();
-        }
-        private void ListenForClients()
-        {
-            this.tcpListener.Start();
-            while (true)
-            {
-                TcpClient client = this.tcpListener.AcceptTcpClient();
-                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
-                clientThread.Start(client);
-            }
-        }
-        public static void HandleClientComm(object client)
-        {
-            TcpClient tcpClient = (TcpClient)client;
-            NetworkStream clientStream = tcpClient.GetStream();
-
-            UTF8Encoding encoder = new UTF8Encoding();
-            string commandToSend;
-            commandToSend = "info::" + _artist + "~" + _song + "~" + _album + "~" + _year;
-            byte[] sendBuffer = encoder.GetBytes(commandToSend);
-            byte[] receiveBuffer = new byte[4096];
-            //int bytesRead;
-            while (true)
+            do
             {
                 try
                 {
-                    if (_songChanged == true)
-                    {
-                        commandToSend = "info::" + _artist + "~" + _song + "~" + _album + "~" + _year;
-                        sendBuffer = encoder.GetBytes(commandToSend);
-                        clientStream.Write(sendBuffer, 0, sendBuffer.Length);
-                        clientStream.Flush();
-
-                        sendBuffer = encoder.GetBytes("image::" + _imageData);
-                        clientStream.Write(sendBuffer, 0, sendBuffer.Length);
-                        clientStream.Flush();
-                        _songChanged = false;
-                    }
-                    
-                    //bytesRead = clientStream.Read(receiveBuffer, 0, 4096);
-                    //string reply = encoder.GetString(receiveBuffer);
-                    //if (reply == "OK")
-                    //    _songChanged = false;
+                    _clientConnected.Reset();
+                    _listener.BeginAcceptSocket(new AsyncCallback(AcceptSocketCallback), null);
+                    _clientConnected.WaitOne();
                 }
                 catch
                 {
-                    break;
                 }
+            } while (true);
+        }
+
+        private static void AcceptSocketCallback(IAsyncResult result)
+        {
+            if (_isStopping) return;
+            try
+            {
+                _clientSocket = _listener.EndAcceptSocket(result);
+                string address = ((IPEndPoint)_clientSocket.RemoteEndPoint).Address.ToString();
+                if (string.Compare(address, "127.0.0.1", StringComparison.Ordinal) != 0)
+                {
+                    bool matched = false;
+                    //    if (UserConfiguration.SystemOptions.ExternalHosts != null) {
+                    //        for (int index = 0; index <= UserConfiguration.SystemOptions.ExternalHosts.Count - 1; index++) {
+                    //            if (string.Compare(address, UserConfiguration.SystemOptions.ExternalHosts(index), StringComparison.Ordinal) == 0 || string.Compare(UserConfiguration.SystemOptions.ExternalHosts(index), "0.0.0.0", StringComparison.Ordinal) == 0) {
+                    //                matched = true;
+                    //                break;
+                    //            }
+                    //        }
+                    //    }
+                    if (!matched) return;
+                }
+                _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes("100 MusicBee\n"));
+                byte[] buffer = new byte[4096];
+                bool connectionClosing = false;
+                int count = 0;
+                do
+                {
+                    int eocIndex = -1;
+                    try
+                    {
+                        do
+                        {
+                            if (_clientSocket.Poll(-1, SelectMode.SelectRead))
+                            {
+                                int bytesRead = _clientSocket.Receive(buffer, count, 4096 - count, SocketFlags.None);
+                                if (bytesRead == 0)
+                                {
+                                    connectionClosing = true;
+                                    break;
+                                }
+                                count += bytesRead;
+                                eocIndex = Array.LastIndexOf<byte>(buffer, 10, count - 1);
+                            }
+                        } while ((count < 4096) && (eocIndex == -1));
+                    }
+                    catch
+                    {
+                        connectionClosing = true;
+                    }
+                    string[] commands;
+                    if (count == 0)
+                    {
+                        commands = new string[20];
+                    }
+                    else
+                    {
+                        commands =
+                            System.Text.Encoding.UTF8.GetString(buffer, 0, count).Replace("\r\n", "\n").Split('\n');
+                    }
+                    foreach (string commandLine in commands)
+                    {
+                        try
+                        {
+                            switch (commandLine)
+                            {
+                                case "NEXT":
+                                    _plugin.PlayNextTrack();
+                                    _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes("250 OK\n"));
+                                    break;
+                                case "PLAYPAUSE":
+                                    _plugin.PlayPauseTrack();
+                                    _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes("250 OK\n"));
+                                    break;
+                                case "PREVIOUS":
+                                    _plugin.PlayPreviousTrack();
+                                    _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes("250 OK\n"));
+                                    break;
+                                case "INCREASEVOL":
+                                    _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes(String.Format("260 VOL UP:{0}\n", _plugin.IncreaseVolume())));
+                                    break;
+                                case "DECREASEVOL":
+                                    _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes(String.Format("270 VOL DOWN:{0}\n", _plugin.DecreaseVolume())));
+                                    break;
+                                    
+                                default:
+                                    break;
+
+
+                            }
+                        }
+                        catch (ThreadAbortException ex)
+                        {
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                _clientSocket.Send(System.Text.Encoding.UTF8.GetBytes("Error"));
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                    if (eocIndex == -1 || eocIndex == count - 1)
+                    {
+                        count = 0;
+                    }
+                    else
+                    {
+                        int remainder = count - (eocIndex + 1);
+                        Array.Copy(buffer, eocIndex + 1, buffer, 0, remainder);
+                        count = remainder;
+                    }
+                } while (!connectionClosing);
             }
-            tcpClient.Close();
+            catch
+            {
+            }
+            finally
+            {
+                if (_clientSocket != null)
+                {
+                    try
+                    {
+                        _clientSocket.Close();
+                    }
+                    catch
+                    {
+                    }
+                    _clientSocket = null;
+                }
+                _clientConnected.Set();
+            }
         }
     }
 }
---
+++ mb_remote.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MusicBeePlugin
+{
+    public partial class Plugin
+    {
+        private MusicBeeApiInterface _mbApiInterface;
+        private readonly PluginInfo _about = new PluginInfo();
+        private SocketServer _mbSoc;
+
+        public PluginInfo Initialise(IntPtr apiInterfacePtr)
+        {
+            _mbApiInterface = (MusicBeeApiInterface)Marshal.PtrToStructure(apiInterfacePtr, typeof(MusicBeeApiInterface));
+            _about.PluginInfoVersion = PluginInfoVersion;
+            _about.Name = "MusicBee Remote Control";
+            _about.Description = "A plugin to allow music bee remote controll through mobile applications and network.";
+            _about.Author = "Kelsos";
+            _about.TargetApplication = "MusicBee Remote";   // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
+            _about.Type = PluginType.General;
+            _about.VersionMajor = 1;  // your plugin version
+            _about.VersionMinor = 0;
+            _about.Revision = 1;
+            _about.MinInterfaceVersion = MinInterfaceVersion;
+            _about.MinApiRevision = MinApiRevision;
+            _about.ReceiveNotifications = ReceiveNotificationFlags.PlayerEvents;
+            _about.ConfigurationPanelHeight = 10;   // not implemented yet: height in pixels that musicbee should reserve in a panel for config settings. When set, a handle to an empty panel will be passed to the Configure function
+            _mbSoc = new SocketServer(this);
+            SocketServer.Start();
+            return _about;
+        }
+
+        public bool Configure(IntPtr panelHandle)
+        {
+            // save any persistent settings in a sub-folder of this path
+            var dataPath = _mbApiInterface.Setting_GetPersistentStoragePath();
+            return false;
+        }
+
+        // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
+        public void Close(PluginCloseReason reason)
+        {
+            SocketServer.Stop();
+        }
+
+        // uninstall this plugin - clean up any persisted files
+        public void Uninstall()
+        {
+        }
+
+        // receive event notifications from MusicBee
+        // only required if about.ReceiveNotificationFlags = PlayerEvents
+        public void ReceiveNotification(string sourceFileUrl, NotificationType type)
+        {
+            // perform some action depending on the notification type
+            switch (type)
+            {
+                case NotificationType.PluginStartup:
+                    // perform startup initialisation
+                    switch (_mbApiInterface.Player_GetPlayState())
+                    {
+                        case PlayState.Playing:
+                        case PlayState.Paused:
+                            // ...
+                            break;
+                    }
+                    break;
+                case NotificationType.TrackChanged:
+
+
+                    // ...
+                    break;
+            }
+        }
+
+        // return lyrics for the requested artist/title
+        // only required if PluginType = LyricsRetrieval
+        // return null if no lyrics are found
+        public string RetrieveLyrics(string sourceFileUrl, string artist, string trackTitle, string album, bool synchronisedPreferred)
+        {
+            return null;
+        }
+
+        // return Base64 string representation of the artwork binary data
+        // only required if PluginType = ArtworkRetrieval
+        // return null if no artwork is found
+        public string RetrieveArtwork(string sourceFileUrl, string albumArtist, string album)
+        {
+            //Return Convert.ToBase64String(artworkBinaryData)
+            return null;
+        }
+        public void PlayNextTrack()
+        {
+            _mbApiInterface.Player_PlayNextTrack();
+        }
+
+        public void PlayPauseTrack()
+        {
+            _mbApiInterface.Player_PlayPause();
+        }
+        public void PlayPreviousTrack()
+        {
+            _mbApiInterface.Player_PlayPreviousTrack();
+        }
+
+        public int IncreaseVolume()
+        {
+            float vol = (float)Math.Round(_mbApiInterface.Player_GetVolume() + (float)0.1, 1);
+            _mbApiInterface.Player_SetVolume(vol);
+            return (int)Math.Round(_mbApiInterface.Player_GetVolume() * 10, 1);
+            
+        }
+        public int DecreaseVolume()
+        {
+            float vol = (float)Math.Round(_mbApiInterface.Player_GetVolume() - (float)0.1, 1);
+            _mbApiInterface.Player_SetVolume(vol);
+            return (int)Math.Round(_mbApiInterface.Player_GetVolume() * 10, 1);
+        }
+    }
+}
