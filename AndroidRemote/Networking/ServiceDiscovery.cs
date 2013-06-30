37e92c28f0bd06dc3cd52824fa385c15778c5884
Konstantinos <kelsos86@gmail.com>
2013-06-30 10:07:06 +0300

    Adds udp multicast listener for service discovery and automatic configuration.

--- AndroidRemote/Networking/ServiceDiscovery.cs
+++ AndroidRemote/Networking/ServiceDiscovery.cs
@@ -1,41 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using MusicBeePlugin.AndroidRemote.Settings;
+using MusicBeePlugin.AndroidRemote.Utilities;
+using MusicBeePlugin.Tools;
+using ServiceStack.Text;
 
 namespace MusicBeePlugin.AndroidRemote.Networking
 {
     class ServiceDiscovery
     {
-        private const int UdpPort = 45345;
-        private static readonly IPAddress MulticastAddress = IPAddress.Parse("224.0.1.10");
+        private const int mPort = 45345;
+        private static readonly IPAddress MulticastAddress = IPAddress.Parse("239.1.5.10");
+        private UdpClient mListener;
+        private readonly static ServiceDiscovery Disc = new ServiceDiscovery();
+
+        public static ServiceDiscovery Instance
+        {
+            get { return Disc; }
+        }
+
+        private ServiceDiscovery()
+        {
+        }
 
         public void Start()
         {
-            UdpClient listener = new UdpClient(UdpPort);
-            IPEndPoint mCastGroupEP = new IPEndPoint(MulticastAddress,UdpPort);
-
-            try
-            {
-                listener.JoinMulticastGroup(MulticastAddress);
-                while (true)
-                {
-                    byte[] bytes = listener.Receive(ref mCastGroupEP);
-                    listener.EnableBroadcast = true;
-                    Debug.WriteLine(Encoding.UTF8.GetString(bytes));
-                    byte[] data = Encoding.UTF8.GetBytes("testtae");
-          
-                    listener.Send(data, data.Length);
-
-                }
-            }
-            catch (Exception)
-            {
-                
-                throw;
-            }
+            mListener = new UdpClient(mPort, AddressFamily.InterNetwork) {EnableBroadcast = true};
+            mListener.JoinMulticastGroup(MulticastAddress);
+            mListener.BeginReceive(OnDataReceived, null);
         }
-      
+
+        private void OnDataReceived(IAsyncResult ar)
+        {
+            IPEndPoint mEndPoint = new IPEndPoint(IPAddress.Any, mPort);
+            byte[] request = mListener.EndReceive(ar, ref mEndPoint);
+            string mRequest = Encoding.UTF8.GetString(request);
+            JsonObject incoming = JsonObject.Parse(mRequest);
+
+            if (incoming.Get("context").Contains("discovery"))
+            {
+                List<string> addresses = NetworkTools.GetPrivateAddressList();
+                string clientAddress = incoming.Get("address");
+                string interfaceAddress = String.Empty;
+                int minDistance = 9001; // It's over nine thousand... 
+                foreach (string address in addresses)
+                {
+                    int distance = LevenshteinDistance.CalculateDistances(clientAddress, interfaceAddress);
+                    if (minDistance > distance)
+                    {
+                        minDistance = distance;
+                        interfaceAddress = address;
+                    }
+                }
+
+                Dictionary<string, object> notify = new Dictionary<string, object>();
+                notify.Add("context","notify");
+                notify.Add("address", interfaceAddress);
+                notify.Add("computer", Environment.GetEnvironmentVariable("COMPUTERNAME"));
+                notify.Add("port", UserSettings.Instance.ListeningPort);
+                byte[] response = Encoding.UTF8.GetBytes(JsonSerializer.SerializeToString(notify));
+                mListener.Send(response, response.Length, mEndPoint);                      
+            }
+            mListener.BeginReceive(OnDataReceived, null);
+        }
+
     }
 }
