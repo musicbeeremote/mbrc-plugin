1fdb3c9226bc01209f1ab86a350525306eddc5d9
Konstantinos Paparas <kelsos86@gmail.com>
2017-04-26 21:18:01 +0300

    Adds initial podcast subscription model

---
+++ mbrc-core/Core/Podcasts/PodcastSubscription.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace MusicBeeRemote.Core.Podcasts
+{
+    [DataContract]
+    public class PodcastSubscription
+    {
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+    }
+}
