24e61424eb799e032a2148eb7eced9d2aa856f44
Konstantinos Paparas <kelsos86@gmail.com>
2020-03-30 11:05:17 +0300

    Disables Costura compression and bundling debug symbols

--- build.bat
+++ build.bat
@@ -14,6 +14,7 @@ rmdir %cd%\build\bin\%config% /s /q
 
 REM Package restore
 tools\nuget.exe restore mbrc-core\packages.config -OutputDirectory %cd%\packages -NonInteractive
+tools\nuget.exe restore plugin\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" MBRC.sln /p:Configuration="%config%";Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
