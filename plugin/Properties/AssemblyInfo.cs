1cd7fff100fadff4e9844bdaeec59139f727e212
Dan Z <dandepeched@gmail.com>
2020-04-10 17:18:38 +0300

    updated current version to 1.2.0

--- plugin/Properties/AssemblyInfo.cs
+++ plugin/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.1456")]
+[assembly: AssemblyVersion("1.2.0")]
