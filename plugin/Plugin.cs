be73e2b9fc02afe871c054aa448a53313391bf60
Konstantinos Paparas <kelsos86@gmail.com>
2020-04-28 23:36:01 +0300

    Updates language to latest
    Introduces FxCopAnalyzers, StyleCopAnalyzers

--- plugin/Plugin.cs
+++ plugin/Plugin.cs
@@ -154,7 +154,7 @@ namespace MusicBeePlugin
                 case NotificationType.NowPlayingArtworkReady:
                     _musicBeeRemotePlugin.NotifyArtworkReady();
                     break;
-                case NotificationType.NowPlayingListChanged:
+                case NotificationType.PlayingTracksChanged:
                     _musicBeeRemotePlugin.NotifyNowPlayingListChanged();
                     break;
             }
